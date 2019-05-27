471c11dd453080103aa4455aa61fce4cf781cdf3
Angela Munayo <ligoangela.16@gmail.com>
2019-05-27 10:14:54 +0000

    t

--- angela.py
+++ angela.py
@@ -2,7 +2,12 @@ def first():
     a=100
     b=120
     c=a+b
+<<<<<<< HEAD
 def mysecondfun():
     print('mine')
 def angela():
     print('Not yet done')
+=======
+def mysecond():
+    k=1000
+>>>>>>> branchone
